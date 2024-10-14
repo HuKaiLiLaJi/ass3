4f1749ccc03ad5e89e1e91049d9bbc8f58348442
yliu0585 <146091202+HuKaiLiLaJi@users.noreply.github.com>
2024-10-14 13:40:40 +0800

    update in week11

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,25 @@
   </header>
 
   <main>
-    <!--LibraryRegistrationForm /-->
-    <!-- <JSONLab /> -->
     <router-view></router-view>
   </main>
+  <footer v-if="showHeader">
+    <BFooter></BFooter>
+  </footer>
 </template>
 
 <script>
-import BHeader from './components/BHeader1.vue'
+import BHeader from './components/BHeader.vue'
+import BFooter from './components/Footer.vue';
 
 export default {
   name: 'APP',
   components: {
-    BHeader
+    BHeader, BFooter
   },
   computed: {
     showHeader() {
-      return (this.$route.name !== 'CountBookAPI' && this.$route.name !== 'GetAllBookAPI');
+      return (this.$route.name !== 'UserAPI' && this.$route.name !== 'GetAllBookAPI');
     }
   }
 }
@@ -29,30 +31,7 @@ export default {
 
 
 <style scoped>
-/* header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  main {
+    min-height: 90%;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
